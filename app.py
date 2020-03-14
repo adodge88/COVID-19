228c4162726197e13b957e0601c83d25634d6733
Marina Puig de Morales Marinković <mpmarinkovic@gmail.com>
2020-03-14 15:11:37 -0400

    logic2 with the circles

--- app.py
+++ app.py
@@ -3,18 +3,31 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, MetaData
 from flask import Flask, jsonify
 
-pg_connection_string = "postgresql://postgres:postgres@localhost:5432/COVID19_DB"
-engine = create_engine(pg_connection_string)
-session = Session(engine)
 
-m = MetaData()
-Base = automap_base(bind=engine, metadata=m)
-Base.prepare(engine, reflect=True)
-covid19 = Base.classes.covid19
 
 app = Flask(__name__)
 
+def get_rss_news():
+    url= "https://www.google.com/alerts/feeds/07584845844048270854/8369353730257505193"
+    feed = feedparser.parse(url)
+    feeds_list = []
+    for post in feed.entries:
+        date = "%02d-%02d-%02d" % (post.published_parsed.tm_mon, \
+            post.published_parsed.tm_mday, \
+            post.published_parsed.tm_year)     
+        post.title = post.title.replace('<b>', '')
+        post.title = post.title.replace('</b>', '')
+        post.title = post.title.replace('&#39;', "'")
+        d = {
+            'date': date,
+            'title': post.title,
+            'link': post.link
+        }
+        feeds_list.append(d)
+    return feeds_list
+
 @app.route("/")
+
 def home():
     results = session.query(covid19.id_loc, covid19.province_state, covid19.country_region,
     covid19.lat, covid19.long, covid19.date, covid19.confirmed_to_date, covid19.deaths_to_date, covid19.recovered_to_date).all()
--- assets/js/logic2.js
+++ assets/js/logic2.js
@@ -1,122 +1,166 @@
 
-// Funtion to scale the marker size
-function  markerSize (mag) {
-  return mag*50000;
-};
-// Creating map object
-var myMap = L.map("map", {
-  center: [40.7, -73.95],
-  zoom: 11
-});
-
 // retrieve the data from the csv file
 d3.csv("assets/data/covid19.csv", function(covid19){
-  label='2020-01-22'
-  // console.log(covid19)
+  // console.log(covid19);
+
   // parse the data 
   covid19.forEach (function(data){
     // console.log(data)
-    data.id_loc = parseInt(data.id_loc)
+    data.id_loc = parseInt(data.id_loc);
+    data.confirmed_to_date = parseInt(data.confirmed_to_date);
+    data.lat = parseFloat(data.lat);
+    data.long = parseFloat(data.long);
   })
 
-  // filter the data by the "label" that in this case is the date
-  var filteredData = covid19.filter(obj => {
-    return obj.date === label
-  })
-  // console.log(filteredData)
-
-  // create corresponding marker arrays for the three classes
-  var markerArrayRecovered = [];
-  var markerArrayDeath = [];
-  var markerArrayConfirmed = [];
-
-  for (var i=0; i < filteredData.length ; i++){
-    markerArrayRecovered.push(
-      L.circle([filteredData[i].lat, filteredData[i].long]), {
-      color:"green",
-      fillColor:"green",
-      stroke: true,
-      fillOpacity: 0.5,
-      weight: 0.5,
-      radius:filteredData[i].recovered_to_date
-    })
-    markerArrayDeath.push(
-      L.circle([filteredData[i].lat, filteredData[i].long]), {
-      color:"red",
-      fillColor:"red",
-      stroke: true,
-      fillOpacity: 0.5,
-      weight: 0.5,
-      radius:filteredData[i].death_to_date
-    });
-    markerArrayConfirmed.push(
-      L.circle([filteredData[i].lat, filteredData[i].long]), {
-      color:"blue",
-      fillColor:"blue",
-      stroke: true,
-      fillOpacity: 0.5,
-      weight: 0.5,
-      radius:filteredData[i].confirmed_to_date
-    })
-  }
-      // Add a popup with information
-      // (filteredData, {
-      //     onEachFeature: function onEachFeature(feature, layer) {
-      //         // content = `${feature.properties.content} <br> (${Math.round(value/6 * 100)}% done with story)`
-      //         // var popup = L.popup().setContent(content);
-      //         // layer.bindPopup(popup);
-      //         markerArray.push(layer);
-      //     }
-      // });
-      
-    var markerGroup = L.featureGroup(markerArrayRecovered,markerArrayDeath,markerArrayConfirmed );
-    L.control.timelineSlider({
-      timelineItems: covid19.date, 
-      changeMap: getDataAddMarkers,
-      // extraChangeMapParams: {exclamation: "?ADD TEXT"} 
-    })
-  // .addTo(mymap);   
-      
-    createMap(covid19)
-})
-  
-
-
-function createMap(covid19) {
-  // Define streetmap and darkmap layers
-  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
+  // Create map object
+  var myMap = L.map("map", {
+    center: [36.8210, 5.7634],
+    zoom: 2,
+    scrollWheelZoom: false
   });
-  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+
+  // Add darkmap tile layer to map
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.dark",
     accessToken: API_KEY
-  });
-  // Define a baseMaps object to hold our base layers
-  var baseMaps = {
-    "Dark Map": darkmap,
-    "Ligth Map": streetmap
-  };
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    TotalCases: covid19
-  };
-  // Create our map, giving it the streetmap and cumulative cases(covid19) layers to display on load
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 2,
-    layers: [darkmap, covid19]
-  });
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
   }).addTo(myMap);
-}
+
+  //Create function to change layer with change of time slider
+  getDataAddMarkers = function( {label, value, map, exclamation} ) {
+
+    //Remove existing layer when change to time slider
+    map.eachLayer(function (layer) {
+      if (layer instanceof L.Marker) {
+        map.removeLayer(layer);
+      }
+    });
+
+    // filter the data by the "label" that in this case is the date
+    var filteredData = covid19.filter(obj => {
+      return obj.date === label
+    })
+    console.log(filteredData);
+
+    // create marker array for the confirmed cases
+    var markerArrayConfirmed = [];
+
+    for (var i=0; i < filteredData.length ; i++){
+      if (filteredData[i].confirmed_to_date > 0) {
+        // markerArrayConfirmed.push(
+          L.circle([filteredData[i].lat,filteredData[i].long], {
+            color:"yellow" ,
+            fillColor: markerColor(filteredData[i].confirmed_to_date),
+            stroke: true,
+            fillOpacity: 0.5,
+            weight: 0.5,
+            radius:markerSize(filteredData[i].confirmed_to_date)
+          })
+
+          .bindPopup("<h3>" + filteredData[i].province_state + "</h3> <h3>" + 
+            filteredData[i].country_region + "</h3> <hr> <h3>" + filteredData[i].date + 
+            "</h3> <hr> <h3>Confirmed: " + filteredData[i].confirmed_to_date + 
+            "</h3> <h3>Deaths: " + filteredData[i].deaths_to_date + 
+            "</h3> <h3>Recovered: " + filteredData[i].recovered_to_date)
+          .addTo(myMap)
+        // )
+      }
+    }
+
+    // console.log(markerArrayConfirmed);
+
+  }
+
+  L.control.timelineSlider({
+    timelineItems: ["2020-01-22", "2020-01-25", "2020-01-29","2020-02-01","2020-02-05", "2020-02-09", "2020-02-13", "2020-02-17", "2020-02-21", "2020-02-25", "2020-02-29", "2020-03-02", "2020-03-05"],
+    changeMap: getDataAddMarkers,
+    // extraChangeMapParams: {exclamation: "?ADD TEXT"} 
+  }).addTo(myMap);
+
+// Funtion to scale the marker size
+function  markerSize (mag) {
+  if (mag > 8000 ){
+    return mag*10 }
+    else {return mag*100}
+};
+// Funtion to color the marker based on size
+function  markerColor (mag) {
+  if (mag > 8000 ){
+    return "red" }
+    else {return "yellow"}
+};
+
+
+
+
+// ================== TOP SCORE CARDS ==================
+// Determine the maximum date value in the data
+    var maxDate = d3.max(covid19.map(d=>d.date));
+    console.log("MaxDate",maxDate);
+
+// Extract only the "rows" where date = maxDate
+    var recentDate = covid19.filter(function(d){ return d.date == maxDate });
+
+// Create empty arrays to push the data from the for loop into
+    var confirmedArray = [];
+    var deathArray = [];
+    var recoveredArray = [];
+
+// Loop through the data and extract all the confirmed_to_date, deaths_to_date
+// and recovered_to_date values from the most recent date in the data
+    for (i = 0; i < recentDate.length; i++) {
+        confirmedArray.push(recentDate[i].confirmed_to_date)
+        deathArray.push(recentDate[i].deaths_to_date)
+        recoveredArray.push(recentDate[i].recovered_to_date)
+      }
+     
+// Sum the arrays to obtain a total for confirmed, deaths, and arrays for the most
+// recent date in the data
+    var totalConfirmed = d3.sum(confirmedArray); 
+    console.log("Confirmed:",totalConfirmed);
+    var totalRecovered = d3.sum(deathArray); 
+    console.log("Recovered:",totalRecovered);
+    var totalDeaths = d3.sum(recoveredArray); 
+    console.log("Deaths:",totalDeaths);
+
+
+    d3.select("#confirmed-card").selectAll("div")
+    .insert("h2")
+    .classed("card-title card-num", true) // sets the class of the new H2
+    .text(totalConfirmed);
+
+    d3.select("#recovered-card").selectAll("div")
+    .insert("h2")
+    .classed("card-title card-num", true) // sets the class of the new H2
+    .text(totalRecovered);
+
+    d3.select("#death-card").selectAll("div")
+    .insert("h2")
+    .classed("card-title card-num", true) // sets the class of the new H2
+    .text(totalDeaths);
+
+
+// ================== DATA DATE ==================
+
+d3.select("footer").selectAll("span")
+.insert("p")
+// .classed("card-title card-num", true) // sets the class of the new H2
+.text(`Most Recent Data from: ${maxDate} [YYYY/MM/DD]`);
+
+});
+
+// ================== LEFT COUNT CARD ==================
+// import new csv of grouped coutnries w/ total confirmed_to_date count
+d3.csv("assets/data/confirmed_country_count.csv", function(data){
+  // Use D3 to select the table body
+  var tbody = d3.select("tbody");
+  //loop through csv and push to HTML table
+  data.forEach((country) =>{
+      var row = tbody.append("tr");
+      Object.entries(country).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+      });
+  });
+});
